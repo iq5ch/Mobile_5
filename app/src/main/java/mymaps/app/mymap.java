285f27bdacb2c74f0ace34c235c7910aa02b72a1
iq5ch <58838542+iq5ch@users.noreply.github.com>
2020-05-01 10:42:59 +0700

    Update mymap.java

--- app/src/main/java/mymaps/app/mymap.java
+++ app/src/main/java/mymaps/app/mymap.java
@@ -40,6 +40,7 @@ public class mymap extends FragmentActivity implements OnMapReadyCallback {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]
                     {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
         }
         Task<Location> task = fusedLocationProviderClient.getLastLocation();
         task.addOnSuccessListener(new OnSuccessListener<Location>() {
